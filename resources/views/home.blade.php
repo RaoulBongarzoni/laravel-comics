ef0c0720d1c11b4918935138f0e1cf69609839b5
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2024-01-25 20:23:04 +0100

    minor changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,13 +12,47 @@
     <div class="cardDisplayer">
         @foreach ($fumetti as $fumetto)
         <div class="card">
-            <img src="{{$fumetto['thumb']}}" alt="">
+            <img class="comicCard" src="{{$fumetto['thumb']}}" alt="">
             <h3>{{$fumetto['title']}}</h3>
+
         </div>
         @endforeach
     </div>
 
 </section>
+
+
+<style scoped>
+    .cardDisplayer {
+        position: relative;
+        height: 100%;
+
+        display: flex;
+        justify-content: space-evenly;
+        margin: 0 auto;
+        align-items: center;
+        flex-wrap: wrap;
+
+    }
+
+    .card {
+        width: 20%;
+        display: flex;
+        flex-direction: column;
+        justify-items: center;
+        align-items: center;
+
+        margin: 1rem
+    }
+
+    .comicCard {
+        width: 100%;
+        height: 100%;
+        aspect-ratio: 1;
+        object-fit: cover;
+
+    }
+</style>
 
 
 @endsection
